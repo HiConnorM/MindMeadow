26289b114e76a53fede1cd9e73eb7ba845111369
ConnorM <theconnorm@gmail.com>
2023-10-04 14:55:51 -0500

    added aws cognito for auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mindmeadow27a70b5e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
+import Amplify from 'aws-amplify';
+import {withAuthenticator} from '@aws-amplify/ui-react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_rIKNLtDXF',
+    identityPoolId: 'us-east-1:ba88a145-e9d6-4a13-aef2-667be5d7f5f2',
+    userPoolWebClientId: '15cr5ur9o1hmjjbeu6n890vbmj',
+  },
+});
+
+// Wrap the App component with the withAuthenticator Higher Order Component
+const AuthenticatedApp = withAuthenticator(App);
+
+// Register the AuthenticatedApp instead of App
+AppRegistry.registerComponent(appName, () => AuthenticatedApp);
